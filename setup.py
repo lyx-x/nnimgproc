1fa1b04f921f685bd14cf74986edf6803ad1efeb
lyx-x <li.yuxiang.nj@gmail.com>
2017-11-18 14:54:16 +0000

    Add setup.py and basic dependencies

---
+++ setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+
+setup(
+    name='nnimgproc',
+    version=1.0,
+
+    packages=['nnimgproc'],
+
+    url='https://github.com/lyx-x/nnimgproc',
+    license='MIT',
+    author='Yuxiang Li',
+    author_email='li.yuxiang.nj@gmail.com',
+    description='Framework for learning image processing task',
+
+    install_requires=[
+        'keras',
+        'scikit-image',
+    ],
+)
